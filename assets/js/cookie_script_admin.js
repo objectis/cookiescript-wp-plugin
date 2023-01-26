798c60bffffca3c70dacfa6c508448d882666352
Arturas <arturas19890228@gmail.com>
2023-01-26 13:40:26 +0200

    updated cookie script plugin

---
+++ assets/js/cookie_script_admin.js
@@ -0,0 +1,83 @@
+(function() {
+
+    jQuery(document).ready(function($) {
+
+        var that,
+            url,
+            itemId = $('#cookie_script_item_id').val(),
+            srcUrl = $('#cookie_script_item_src').val();
+
+        $('#cookie_script_item_src').change('change keydown keyup click',  function() {
+
+            that = $(this),
+            itemId = $(this).val(),
+            url = srcUrl;
+
+            if (srcUrl) != null) ) {
+
+                $.ajax({
+                    url: url,
+                    type: 'get',
+                    success: function(d, s, x) {
+                        that.removeClass('CookieScript--error').addClass('CookieScript--success');
+                        $('.CookieScript__adminForm--footer').find('span').text( cookie_script_admin_exchanger.notice_success );
+                    },
+                    error: function(d, s, x) {
+                         that.removeClass('CookieScript--success').addClass('CookieScript--error');
+                         $('.CookieScript__adminForm--footer').find('span').text( cookie_script_admin_exchanger.notice_error );
+                    }
+                });
+
+            }
+
+            that.removeClass('CookieScript--success').addClass('CookieScript--error');
+
+        })
+
+        //TODO need to remove this part of code on next update
+        $('.CookieScript__connectionType').find('label').on('click', function() {
+
+            that = $(this);
+
+            switch ( that.find('input').val() ) {
+                case '1':
+                    url = 'http://chs03.cookie-script.com/s/' + itemId + '.js';
+                    break;
+                case '2':
+                    url = 'https://cookie-script.com/s/' + itemId + '.js';
+                    break;
+                case '3':
+                    url = 'https://eu.cookie-script.com/s/' + itemId + '.js';
+                    break;
+            }
+
+            $.ajax({
+                url: url,
+                type: 'get',
+                success: function(d, s, x) {
+                    $('.CookieScript__connectionType').find('label').removeClass();
+                    $('.CookieScript__connectionType').find('label').find('input').removeAttr('disabled');
+                    that.removeClass('CookieScript--error').addClass('CookieScript--success');
+                    that.find('input').attr('checked', true);
+                    $('.CookieScript__adminForm--footer').find('span').text( cookie_script_admin_exchanger.notice_success );
+                },
+                error: function(d, s, x) {
+                    if ( that.attr('for') == "cookie_script_item_connection_type_2" ) {
+                        $('.CookieScript__adminForm--footer').find('span').html( cookie_script_admin_exchanger.notice_error_https );
+                    } else if ( that.attr('for') == "cookie_script_item_connection_type_3" ) {
+                        $('.CookieScript__adminForm--footer').find('span').html( cookie_script_admin_exchanger.notice_error_geo );
+                    } else {
+                        $('.CookieScript__adminForm--footer').find('span').text( cookie_script_admin_exchanger.notice_error );
+                    }
+                    $('.CookieScript__connectionType').find('label').removeClass();
+                    $('.CookieScript__connectionType').find('label').find('input').removeAttr('disabled');
+                    that.removeClass('CookieScript--success').addClass('CookieScript--error');
+                    that.find('input').removeAttr('checked').attr('disabled', true);
+                }
+            });
+
+        });
+
+    });
+
+})();
---
+++ readme.txt
@@ -0,0 +1,43 @@
+﻿=== Cookie-Script.com ===
+Contributors: cookiescriptcom, fujiii
+Tags: gdpr, cookie, cookies, compliance, cookie script, plugin, cookie consent, consent, eu, europe
+Requires at least: 4.9.8
+Tested up to: 6.1
+Stable tag: 0.6
+Requires PHP: 5.2 or higher
+License: MIT License (Expat)
+License URI: https://tldrlegal.com/license/mit-license
+
+Cookie-Script.com WordPress plugin.
+
+== Description ==
+Cookie Script helps you to comply with European Cookie Law and GDPR. Make your own cookie information popup, no programming skills required. You can even use it for free.
+
+== Frequently Asked Questions ==
+It would be best if you checked Cookie-Script.com manuals section before:
+https://cookie-script.com/blog/user-manuals/
+
+For now this plugin only helps to insert Cookie-Script.com code inside your WordPress website in an easy, user-friendly way, without the need to edit template files.
+
+Blocks first-party cookies out of the box.
+
+To block third-party cookies you still have to make these changes:
+https://cookie-script.com/how-to-block-third-party-cookies.html
+
+For help and support, any kind of suggestions feel free to write at:
+support@cookie-script.com
+
+== Screenshots ==
+1. Cookie-Script.com plugin settings page
+
+== Changelog ==
+0.6 - Tested on a WordPress 6.1 version. Works as intended.
+0.5 - Tested on a WordPress 5.8 version. Works as intended.
+0.4 - Tested on a WordPress 5.7 version. Works as intended.
+0.3 - Tested on a WordPress 5.6 version. Works as intended.
+0.2 - Tested on a WordPress 5.0 version. Works as intended.
+0.1 - Initial release
+
+== == == == == == == == == == == == == == == == == == == == == == == == == == == ==
+
+(づ｡◕‿‿◕｡)づ https://cookie-script.com/
